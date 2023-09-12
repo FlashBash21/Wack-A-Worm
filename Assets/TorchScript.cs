9f47e20b54e938ca2543f1e3ed6fd84d7525e4cb
Nathan <nathanolson55379@gmail.com>
2023-09-12 09:47:07 -0500

    Initial Commit

---
+++ Assets/TorchScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        target.z = transform.position.z;
+
+        transform.position = Vector2.MoveTowards(transform.position, target, 100 * Time.deltaTime);
+
+        if (Input.GetMouseButton(0))
+        {
+            gameObject.transform.eulerAngles = new Vector3(0, 0, -10);
+        } else
+        {
+            gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        
+    }
+}
